bbe22af6332eff858b68e9d72ff436870880b829
Oom Lout <aaron@oomlout.com>
2022-06-20 13:15:30 +0100

    Added files for second instance.

---
+++ OOMPgitSupportFiles.bat
@@ -0,0 +1,24 @@
+md sourceFiles
+cd sourceFiles
+
+git clone https://github.com/sparkfun/SparkFun-Eagle-Libraries
+cd SparkFun-Eagle-Libraries
+git pull
+cd..
+git clone https://github.com/adafruit/Adafruit-Eagle-Library
+cd Adafruit-Eagle-Library
+git pull
+cd..
+git clone https://gitlab.com/kicad/libraries/kicad-footprints
+cd kicad-footprints
+git pull
+cd..
+git clone https://gitlab.com/kicad/libraries/kicad-symbols
+cd kicad-symbols
+git pull
+cd..
+git clone https://github.com/ContextualElectronics/CE_KiCadLib
+cd CE_KiCadLib
+git pull
+cd..
+cd..
