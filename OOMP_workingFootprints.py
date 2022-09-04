e4470a9abad9b988f79ce8620b9421f7b1adaf6f
Oom Lout <aaron@oomlout.com>
2022-09-04 21:17:09 +0100

    Update OOMP_workingFootprints.py

--- OOMP_workingFootprints.py
+++ OOMP_workingFootprints.py
@@ -5,25 +5,14 @@ import OOMPeda
 from oomBase import *
 import json
         
-#OOMP.loadParts("all")
-#OOMP.exportPickle()
-
 OOMP.loadParts("pickle")
 
-print("Number of Items: "+ str(len(OOMP.getItems("all"))))
-print("Number of Symbols: "+ str(len(OOMP.getItems("symbols"))))
-print("Number of Footprints: "+ str(len(OOMP.getItems("footprints"))))
-print("Number of Parts: "+ str(len(OOMP.getItems("parts"))))
-print("Number of Projects: "+ str(len(OOMP.getItems("projects"))))
+print(OOMP.getReport())
 
-#OOMP.printParts()
 OOMP.setBaseDir("C:/GH/oomlout_OOMP/")
 
 #oomDelay(5)
 
-### ### Single is for doing default libraries
-
-#OOMPeda.harvestEagleLibraries(footprint=True,files=True,single=True, overwrite=False)
 harvestKicadSymbolsFiles = False
 harvestKicadSymbolsImages = True
 harvestEagleLibraries = False
@@ -33,32 +22,3 @@ filter="all"
 filter="symbols"
 OOMPeda.doTasks(overwrite=overwrite,harvestKicadSymbolsFiles=harvestKicadSymbolsFiles, harvestKicadSymbolsImages=harvestKicadSymbolsImages,harvestEagleLibraries=harvestEagleLibraries)
 
-#OOMPeda.eagleSetLibrary("19inch")
-#OOMPeda.eagleResetLibrary()
-
-#import OOMP_genPickle
-
-#OOMPeda.harvestKicadLibraries()
-
-#owner = "kicad-footprints"
-#OOMPEDA.harvestKicadFootprintImages(owner)
-#library="C:/EAGLE 9.6.2/cache/lbr/pinhead.lbr"
-#libraryName="pinhead"
-#OOMPeda.harvestEagleFootprint(library,libraryName)
-
-# search
-## image.png -imageZ1 -imageZ2 -imageZ3 -imagez4 -imagez5 -imagez6 -imagez7 -imagez8 -imagez9 -_140 -_450 -_600
-#OOMPeda.harvestEagleLibraries()
-
-#oomDelay(2)
-#oomMouseScrollWheel(movement=-50)
-
-
-#oomScreenCapture("temp1.png",crop=[560,105,900,900])
-
-
-#json testing
-#item = OOMP.parts[517]
-
-#print(json.dumps(item,default=lambda o: o.__dict__, sort_keys=True, indent=4))
-#print(item.__dict__)
