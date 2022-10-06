b041a3979ae467224eb195e5ca12359d08ca23e7
Oom Lout <aaron@oomlout.com>
2022-10-06 23:17:22 +0100

    Massaging.

--- OOMP_genSingleItem.py
+++ OOMP_genSingleItem.py
@@ -34,6 +34,7 @@ item = OOMP.getPartByID("HEAD-I01-X-PI02-01")
 #C:\GH\oomlout_OOMP\oomlout_OOMP_parts\HEAD-I01-X-PI18-01
 item = OOMP.getPartByID("CAPC-0603-X-NF100-V50")
 #item = OOMP.getPartByID("PROJ-ADAF-1231-STAN-01")
+item = OOMP.getPartByID("PROJ-ELLA-0001-STAN-01")
 #C:\GH\oomlout_OOMP\oomlout_OOMP_projects\PROJ-ADAF-1032-STAN-01
 #item = OOMP.getPartByID("FOOTPRINT-eagle-eagle-default-atmel-SO24W")
 #C:\GH\oomlout_OOMP\oomlout_OOMP_eda\footprints\eagle\eagle-default\atmel\SO24W
--- OOMP_workingProjects2.py
+++ OOMP_workingProjects2.py
@@ -17,21 +17,21 @@ eagleToKicad = False        #kicad launcher open
 kicadProcess = False        #kicad launcher open
 interactiveBom = False
 interactiveBomImages = False
-partsHarvest = False
-matchParts = False
+partsHarvest = True
+matchParts = True
 loadInstances = False
 pcbDraw = False
 matchFootprints = True
 
 filter="projects"
 #filter="projects"
-OOMPprojectLaunch.doTasks(overwrite=overwrite,filter=filter,eagleToKicad=eagleToKicad,kicadProcess=kicadProcess,eagleProcess=eagleProcess,interactiveBom=interactiveBom,interactiveBomImages=interactiveBomImages,partsHarvest=partsHarvest,matchParts=matchParts,loadInstances=loadInstances,pcbDraw=pcbDraw,matchFootprints=matchFootprints)
+#OOMPprojectLaunch.doTasks(overwrite=overwrite,filter=filter,eagleToKicad=eagleToKicad,kicadProcess=kicadProcess,eagleProcess=eagleProcess,interactiveBom=interactiveBom,interactiveBomImages=interactiveBomImages,partsHarvest=partsHarvest,matchParts=matchParts,loadInstances=loadInstances,pcbDraw=pcbDraw,matchFootprints=matchFootprints)
 
 overwrite=True
 project =OOMP.getPartByID("PROJ-ADAF-1032-STAN-01")
 #project =OOMP.getPartByID("PROJ-ADAF-3382-STAN-01")
 #project =OOMP.getPartByID("PROJ-SPAR-10103-STAN-01")
-#project =OOMP.getPartByID("PROJ-SPAR-10616-STAN-01")
+project =OOMP.getPartByID("PROJ-ELLA-0001-STAN-01")
 
 OOMPprojectLaunch.doTask(project=project,overwrite=overwrite,eagleToKicad=eagleToKicad,kicadProcess=kicadProcess,eagleProcess=eagleProcess,interactiveBom=interactiveBom,interactiveBomImages=interactiveBomImages,partsHarvest=partsHarvest,matchParts=matchParts,loadInstances=loadInstances,pcbDraw=pcbDraw,matchFootprints=matchFootprints)
 
