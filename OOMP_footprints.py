d1286c4dd145aaf7959db058f52b4ae1c9dfa9b5
Oom Lout <aaron@oomlout.com>
2022-10-14 13:31:52 +0100

    Makes directory now.

--- OOMP_footprints.py
+++ OOMP_footprints.py
@@ -5,7 +5,7 @@ import OOMP_footprints_KICAD
 OOMP.setBaseDir("C:/GH/oomlout_OOMP/")
 OOMP.loadParts("pickle")
 
-#OOMP_footprints_BASE.gitPull()
+OOMP_footprints_BASE.gitPull()
 
 #OOMP_footprints_BASE.createAllFootprints()
 #OOMP.loadParts("all")
@@ -21,4 +21,4 @@ id = "FOOTPRINT-kicad-kicad-footprints-LED_THT-LED_Rectangular_W3.9mm_H1.8mm"
 footprint = OOMP.getPartByID(id)
 
 #def harvestFootprint(footprint,all=False,copySourceFiles=False,harvestFootprintImages=False,
-OOMP_footprints_KICAD.harvestFootprint(footprint,all=True)
+#OOMP_footprints_KICAD.harvestFootprint(footprint,all=True)
--- OOMP_footprints_BASE.py
+++ OOMP_footprints_BASE.py
@@ -6,6 +6,7 @@ import OOMP_footprints_KICAD
 
 def gitPull():
     dir = "sourceFiles/git/"
+    oomMakeDir(dir)
     ###### Kicad Base Footprints
     gitLoc = "https://gitlab.com/kicad/libraries/kicad-footprints";    oomGitPull(gitLoc,dir)
     ###### oomlout_OOMP_modules    
